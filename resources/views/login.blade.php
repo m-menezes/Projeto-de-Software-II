19ffb0b6b0873b7b752f327a1a6de4cdec73ac8f
Alan Naidon <alannaidon@yahoo.com.br>
2017-09-07 18:19:54 -0300

    Login Layout

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,7 @@
 <head>
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+  <link rel="stylesheet" href="{{asset('css/login.css')}}">
   <meta charset="UTF-8">
   <title>Document</title>
 <style>
@@ -36,28 +37,28 @@ body {
       <form class="login-form">
         <div class="row">
           <div class="input-field col s12 center">
-            <img src="images/login-logo.png" alt="" class="circle responsive-img valign profile-image-login">
-            <p class="center login-form-text">Material Design Admin Template</p>
+            <img class="logo" src="http://w3.ufsm.br/ccsh/modules/mod_ufsm_logo/images/brasao-cores.png" alt="" class="circle responsive-img valign profile-image-login" style="max-width: 120px; max-height: 120px">
+            <p class="teal-text text-darken-2 flow-text center login-form-text">Oportunidades UFSM</p>
           </div>
         </div>
         <div class="row margin">
           <div class="input-field col s12">
             <i class="mdi-social-person-outline prefix"></i>
             <input id="username" type="text">
-            <label for="username" class="center-align">Username</label>
+            <label for="username" class="center-align">Usuário</label>
           </div>
         </div>
         <div class="row margin">
           <div class="input-field col s12">
             <i class="mdi-action-lock-outline prefix"></i>
             <input id="password" type="password">
-            <label for="password">Password</label>
+            <label for="password">Senha</label>
           </div>
         </div>
         <div class="row">          
           <div class="input-field col s12 m12 l12  login-text">
               <input type="checkbox" id="remember-me" />
-              <label for="remember-me">Remember me</label>
+              <label for="remember-me">Salvar Credenciais</label>
           </div>
         </div>
         <div class="row">
@@ -67,10 +68,10 @@ body {
         </div>
         <div class="row">
           <div class="input-field col s6 m6 l6">
-            <p class="margin medium-small"><a href="page-register.html">Register Now!</a></p>
+            <p class="margin medium-small"><a href="page-register.html">Registre-se Agora</a></p>
           </div>
           <div class="input-field col s6 m6 l6">
-              <p class="margin right-align medium-small"><a href="page-forgot-password.html">Forgot password ?</a></p>
+              <p class="margin right-align medium-small"><a href="page-forgot-password.html">Esqueceu sua senha ?</a></p>
           </div>          
         </div>
 
@@ -81,24 +82,8 @@ body {
 
 
   <!-- ================================================
-    Scripts
-    ================================================ -->
-
-  <!-- jQuery Library -->
-  <script type="text/javascript" src="js/plugins/jquery-1.11.2.min.js"></script>
-  <!--materialize js-->
-  <script type="text/javascript" src="js/materialize.min.js"></script>
-  <!--prism-->
-  <script type="text/javascript" src="js/plugins/prism/prism.js"></script>
-  <!--scrollbar-->
-  <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-
-      <!--plugins.js - Some Specific JS codes for Plugin Settings-->
-    <script type="text/javascript" src="js/plugins.min.js"></script>
-    <!--custom-script.js - Add your own theme custom JS-->
-    <script type="text/javascript" src="js/custom-script.js"></script>
-
-  
+                          Scripts
+    ================================================ -->  
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>  
 </body>
